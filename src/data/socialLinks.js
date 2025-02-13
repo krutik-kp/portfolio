e720514476b86962211fd47d30fe8d1e482add58
krutik-kp <krutik5436@gmail.com>
2025-02-13 11:30:55 +0530

    first commit

---
+++ src/data/socialLinks.js
@@ -0,0 +1,33 @@
+export const socialLinks = [
+	// {
+	// 	id: 1,
+	// 	name: 'Website',
+	// 	icon: 'globe',
+	// 	url: 'https://www.stoman.me/',
+	// },
+
+	// {
+	// 	id: 3,
+	// 	name: 'Twitter',
+	// 	icon: 'twitter',
+	// 	url: 'https://twitter.com/realstoman',
+	// },
+	{
+		id: 1,
+		name: 'LinkedIn',
+		icon: 'linkedin',
+		url: 'https://www.linkedin.com/in/krutik-patel-40a29b190/',
+	},
+	{
+		id: 2,
+		name: 'GitHub',
+		icon: 'github',
+		url: 'https://github.com/krutik-kp',
+	},
+	// {
+	// 	id: 5,
+	// 	name: 'YouTube',
+	// 	icon: 'youtube',
+	// 	url: 'https://www.youtube.com/c/realstoman',
+	// },
+];
